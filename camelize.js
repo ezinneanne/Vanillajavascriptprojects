1ec51cb6b159908224d571eac51861fe534ec4e5
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2022-03-30 21:48:29 +0100

    Adding

---
+++ camelize.js
@@ -0,0 +1,30 @@
+/*Translate border-left-width to borderLeftWidth
+importance: 5
+Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+That is: removes all dashes, each word after dash becomes uppercased.
+
+Examples:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+P.S. Hint: use split to split the string into an array, transform it and join back.
+*/
+let camelize = (str) => {
+  return str.split('-').map((word,index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
+}
+console.log(camelize('background-color'))
+
+/*
+function camelize(str) {
+  return str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+*/
