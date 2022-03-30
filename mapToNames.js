d4624602bc6fb3ac6748fe94b9387f7e58c17c8b
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2022-03-30 21:52:20 +0100

    Adding

---
+++ mapToNames.js
@@ -0,0 +1,13 @@
+/*You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+For instance:*
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(item => item.name)
+
+alert( names ); // John, Pete, Mary
