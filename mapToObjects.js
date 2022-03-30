fd4415aefd8b062cebb1110b31fabdfa21a19c6b
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2022-03-30 21:53:03 +0100

    Adding

---
+++ mapToObjects.js
@@ -0,0 +1,40 @@
+/*You have an array of user objects, each one has name, surname and id.
+
+Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+For instance:
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(item => item.name)
+
+
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+//So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
+
+
+
+
+
+/*let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(item => item.name);
+
+alert( names ); // John, Pete, Mary
+*/
